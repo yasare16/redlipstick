0edb86e9fadafb25c93d17e0d6ef1be19b8af2f4
yasare16 <yasare16@gmail.com>
2014-09-04 15:33:05 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,46 @@
-<>Heloow
+<html><head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<title>RedLipstick</title>
+<link rel="stylesheet" href="/style.css">
+<link href="http://fonts.googleapis.com/css?family=Berkshire+Swash|Raleway" rel="stylesheet" type="text/css">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script type="text/javascript">
+	$(document).ready(function() {
+    	
+        });
+</script>
 
+</head>
+
+<body>
+<div class="wrapper">
+    <div class="header" id="head-mast">
+        <div class="grid-1">
+            <a id="logo"><span>Red</span><span>Lipstick</span></a>
+            <ul class="nav">
+                <li><a href="#"></a>
+                </li><li><a href="#"></a>
+                </li><li><a href="#"></a>
+                </li><li><a href="#"></a>
+                </li><li><a href="#"></a>
+            </li></ul>
+        </div>
+    </div>
+    <section class="landing topspace">
+        <div class="grid-1">
+            <div class="grid-2">
+               <h1 class="huge">Choose</h1>
+               <p class="bold raleway caps">your style</p>
+            </div>
+        </div>
+    </section>
+    
+    
+</div>
+<div class="footer">
+
+</div><!--footer>
+
+</body>
+</html>--></body></html>
